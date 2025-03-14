e81c23f18d723d6d493cf108e34610f6129aa3cb
Charis Cat <65819058+ChildOfAnAndroid@users.noreply.github.com>
2025-03-14 16:11:53 +0000

    pointless change

--- infer.py
+++ infer.py
@@ -4,16 +4,16 @@ from config import *
 import torch
 
 def chat(babyLLM, vocab):
-    inputSentence = input("What do you say? ")
-    if len(inputSentence) == 0:
+    userInput = input("What do you say? ")
+    if len(userInput) == 0:
         return
     exitAfter = False 
-    if inputSentence.startswith("!exit"):
+    if userInput.startswith("!exit"):
         exitAfter = True
-        inputSentence = "good bye !" # You have to say goodbye to your helpful AIs before leaving! (or is that only for Luigi boards?)
+        userInput = "good bye !" # You have to say goodbye to your helpful AIs before leaving! (or is that only for Luigi boards?)
 
-    #inputSeq = vocab.nltkTokenizer(inputSentence)
-    inputSeq = vocab.huggingTokenizer(inputSentence)
+    #inputSeq = vocab.nltkTokenizer(userInput)
+    inputSeq = vocab.huggingTokenizer(userInput)
     output = []
     for _ in range(10):
         guessedToken = babyLLM.getNextToken(inputSeq)
@@ -23,10 +23,10 @@ def chat(babyLLM, vocab):
     if exitAfter:
         print(f"Good bye babyLLM! Full response: {" ".join(output)}")
         exit(0)
-    #print(f"You said: \"{inputSentence}\", Full response: {" ".join(output)}")
+    #print(f"You said: \"{userInput}\", Full response: {" ".join(output)}")
     response = ''.join(output).replace('Ġ', ' ').strip() # replace Ġ with space
     response = ' '.join(response.split())  # remove extra spaces
-    print(f'You said: "{inputSentence}"\nBabyLLM says: "{response}"')
+    print(f'You said: "{userInput}"\nBabyLLM says: "{response}"')
 
     
 if __name__ == "__main__":
--- readmeactuallyprobablydont.txt
+++ readmeactuallyprobablydont.txt
@@ -1,1 +1,4 @@
+NEURON LAYER is meant to be outputting a single number for each input token, iterated by numNeurons
+
 PARALLEL NEURON LAYER is meant to be outputting [seqLen, numNeurons]
+
