scaling fixes largely

BRAIN/LAYERS/logits.py
@@ -53,14 +53,15 @@ class LOGITS(nn.Module):
             if debugPrints: print(f"Debug logits: weights shape: {self.l_weights.shape}")
                         
             ʕっʘ‿ʘʔっ("L3: scaledActivations") # <- L1 + L2
-            logitOutput = self.scaledActivations @ self.l_weights + self.l_bias
-            logitNormed = (self.scaledActivations @ self.l_weights) / (numNeurons ** 0.5) + self.l_bias
+            logitOutput = (self.scaledActivations @ self.l_weights / (numNeurons ** 0.5)) + self.l_bias
+            logitOutput = logitOutput.clamp(-60, 60)
+            #logitNormed = (self.scaledActivations @ self.l_weights) / (numNeurons ** 0.5) + self.l_bias
+            logitNormed = self.logitNorm(logitOutput)  # softly smooth
 
             ʕっʘ‿ʘʔっ("L4: logitOutput") # <- L3 (with weights and bias)
             outScale = torch.sigmoid(self.outputScale)
             normOutScale = torch.sigmoid(self.normOutputScale)
             self.finalLogit = (logitOutput * outScale) + (logitNormed * normOutScale)
-            #self.finalLogit = self.finalLogit.clamp(-60, 60) # logits over ~±80 can create inf probs or 0 gradients due to softmax squashing
             if debugPrints: print(f"Debug logits: logitOutput shape AFTER @ weights: {logitOutput.shape}")
 
             ʕっʘ‿ʘʔっ("clamp scalar parameters")
@@ -107,6 +108,9 @@ class LOGITS(nn.Module):
                 #self.stats["6L_1_normedActivationsTensor_scale"] = normedScale.item()
                 self.stats["6L_3_outSigmoid_scale"] = outScale.detach().item()
                 self.stats["6L_4_outNormSigmoid_scale"] = normOutScale.detach().item()
+            if debugPrints: print("activation norm:", self.scaledActivations.norm().item())
+            if debugPrints: print("weight norm mean:", self.l_weights.norm(dim=0).mean().item())
+            if debugPrints: print("weight norm max:", self.l_weights.norm(dim=0).max().item())
 
             # return logits (not softmax) for better gradient computation in cross-entropy loss
             return self.finalLogit # L6 ->

babyLLM.py
@@ -219,6 +219,7 @@ class BABYLLM(nn.Module):
             ʕっʘ‿ʘʔっ("cross Entropy Loss")
             #LOSSlogits = torch.clamp(_logits, min=-50, max=50)
             loss = F.cross_entropy(_logits, targetTensor)
+            self.CEloss_used = loss
 
             if not torch.isfinite(loss):
                 print("NaN/Inf loss detected — logits:", _logits)
@@ -246,17 +247,27 @@ class BABYLLM(nn.Module):
                 repetitionPenaltySoftClamp = 0.00002 * (self.repetitionPenalty - repetitionPenaltyGOAL).pow(2)
 
             loss += lrSoftClamp # use .detach() to avoid .backward()
+            self.lrSoftClamp_used = lrSoftClamp
             loss += tempSoftClamp
+            self.tempSoftClamp_used = tempSoftClamp
             loss += repetitionPenaltySoftClamp
+            self.repPenSoftClamp_used = repetitionPenaltySoftClamp
             self.lastLossBaby = loss.item()
             FINALloss = loss
             #print(f"{FINALloss} + loss")
 
-            if _training and self.lastSoftSample is not None and not skipAuxLoss:
+            if self.lastSoftSample is not None and not skipAuxLoss:
                 target = F.one_hot(targetTensor, num_classes = _logits.shape[1]).float()
-                auxLoss = F.kl_div(self.lastSoftSample.log(), target, reduction = 'batchmean')
-                AUXloss = auxLoss * torch.sigmoid(loss - auxLoss) # low weight for anti-dominatrix
+                AUXloss_kl = F.kl_div(self.lastSoftSample.log(), target, reduction = 'batchmean')
+                self.AUXlossKL_used = AUXloss_kl
+                #AUXloss = auxLoss * torch.sigmoid(loss - auxLoss) # low weight for anti-dominatrix
+                cosSim = F.cosine_similarity(self.lastSoftSample, target)
+                AUXloss_cos = (1.0 - cosSim.mean())
+                self.AUXlossCos_used = AUXloss_cos
+                AUXloss = AUXloss_cos + AUXloss_kl
                 #print(f"{AUXloss} + aux")
+            else:
+                AUXloss = 0
 
             if not skipPixels and (self.nextPixelTarget is not None and hasattr(self, "pixelPupil")):
                 ʕっʘ‿ʘʔっ("RGB regression loss")
@@ -289,13 +300,21 @@ class BABYLLM(nn.Module):
                 # [0-25]x0.1 > 0 > [0-1]
                 # 0-2.5 > 0 > 0-1
             if not skipPixels and (self.nextPixelTarget is not None and hasattr(self, "pixelPupil")): 
-                FINALloss += (self.PIXELloss * 50)
-                self.pixelLoss_used = (self.PIXELloss * 50)
+                FINALloss += (self.PIXELloss)
+                self.pixelLoss_used = (self.PIXELloss)
                 #print(f"{FINALloss} pixel + final")
-            if _training and self.lastSoftSample is not None and not skipAuxLoss: 
+            if self.lastSoftSample is not None and not skipAuxLoss: 
+                if torch.isnan(AUXloss) or not torch.isfinite(AUXloss):
+                    print(f"AUXloss contains NaN!")
+                    AUXloss = torch.tensor(0.0, device = self.device)
                 FINALloss += AUXloss
+            self.AUXloss_used = (AUXloss)
                 #print(f"{FINALloss} aux + final")
             #if debugPrints: print(f"[LOSS DEBUG] requires_grad: {loss.requires_grad} | value: {loss.detach().cpu().item():.4f}")
+            token_freqs = self.lastSoftSample.mean(dim=0)
+            repLoss = (token_freqs**2).mean() * self.repetitionPenalty
+            self.repLoss_used = repLoss
+            FINALloss += repLoss
             return FINALloss
     
     """backpropagation and optimization, computes gradients of the loss and uses the optimizer to update the models weights"""
@@ -376,8 +395,15 @@ class BABYLLM(nn.Module):
                 "B_expWindow": repWindow.item(),
                 "B_repetitionWindow": self.repetitionWindow.item(),
                 "B_temperature": torch.exp(self.logTemp).item(),
-                "B_PIXELloss": self.PIXELloss,
-                "B_PIXELloss_scaled": self.pixelLoss_used
+                "L_CEloss": self.CEloss_used,
+                "L_PIXELloss": self.PIXELloss,
+                "L_PIXELloss_scaled": self.pixelLoss_used,
+                "L_AUXlossCos": self.AUXlossCos_used,
+                "L_AUXlossKL": self.AUXlossKL_used,
+                "L_LRclamp": self.lrSoftClamp_used,
+                "L_tempClamp": self.tempSoftClamp_used,
+                "L_repPenClamp": self.repPenSoftClamp_used,
+                "L_repLoss": self.repLoss_used,
             }
             self.stats.update(self.backwardStats)
             self.pixelLoss_used = 0
@@ -488,7 +514,7 @@ class BABYLLM(nn.Module):
 
             ʕっʘ‿ʘʔっ("repWindow = torch.exp(self.logRepetitionWindow)")
             repWindow = torch.exp(self.logRepetitionWindow)
-            repWindow = repWindow / (1 + repWindow / self.numTokensPerStep)  # asymptotes near windowMAX
+            repWindow = repWindow / (1 + repWindow / self.numTokensPerStep)
             ʕっʘ‿ʘʔっ("penalty = self.repetitionPenalty")
             penalty = self.repetitionPenalty
 
@@ -509,11 +535,12 @@ class BABYLLM(nn.Module):
             ʕっʘ‿ʘʔっ("weightedFreqs = (oneHots.T @ softMask).view(1, -1)")
             weightedFreqs = (oneHots.T @ softMask).view(1, -1)
 
-            # Scale penalty based on confidence (entropy)
-            entropy = -(_logits.softmax(dim=-1) * _logits.log_softmax(dim=-1)).sum(dim=-1, keepdim=True)
-            dynamicPenalty = 0.01 * penalty / (1 + entropy)  # soft & bounded
+            ʕっʘ‿ʘʔっ("setting penalty to 0 for target token!")
+            if self.targetTokenFromTutor is not None:
+                weightedFreqs[0, self.targetTokenFromTutor] = 0.0
 
-        return _logits - (weightedFreqs * dynamicPenalty)
+        return _logits - (weightedFreqs * penalty)
+
 
 
     def getNextToken(self, _inputSeq):  

config.py
@@ -2,6 +2,10 @@
 # --- ʕっʘ‿ʘʔっ --- 
 # BABYLLM CONFIG FILE // config.py
 
+import datetime as CONFIGDATE
+date = CONFIGDATE.date.today()
+
+
 import torch
 modelDevice = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 #modelDevice = torch.device("cpu")
@@ -80,6 +84,7 @@ chatLogPath_infer = "SCHOOL/statistics/LOGS/chat/chatLog.txt"
 chatLogPath_talkToYourself = "SCHOOL/statistics/LOGS/chat/talkToYourselfBattle.txt"
 chatLogPath_talkToYourselfComparisons = "SCHOOL/library/charisStudies/whoIsMoreLikeYou.txt"
 chatLogPath_trainingLog = "SCHOOL/statistics/LOGS/chat/trainingLog_questions.txt"
+babyLogPathFull = f"SCHOOL/statistics/LOGS/chat/babyLogFull_{date}.txt"
 
 """--- VOCAB --- (see master config)"""
 
@@ -89,7 +94,7 @@ chatLogPath_trainingLog = "SCHOOL/statistics/LOGS/chat/trainingLog_questions.txt
 
 """--- --- --- --- --- SETTINGS & CONFIG --- --- --- --- ---"""
 """--- MODEL ---"""
-numTokensPerStepSTART = 1 # Number of tokens to predict per step, // 1024 = crash, 512 is POSSIBLE but its the slowest thing in existence.
+numTokensPerStepSTART = 22 # Number of tokens to predict per step, // 1024 = crash, 512 is POSSIBLE but its the slowest thing in existence.
 perfectionistPassRate = 20
 perfectionistPassRateSTART = 80
 perfectionistMaxRetries = 100
@@ -226,6 +231,19 @@ mostImportantStats  =   [
                 #"5B_x_finalNormLayer_norm",                     # IMPORTANT LAYER TRACKER !! (BABYLLM)
                 #                                                   "7B_x_FINALlogits_norm", # MATCHES 6L_x_finalLogit_norm
                 #"B_floatMemoryLength",
+                "L_CEloss",
+                "L_PIXELloss",
+                "L_PIXELloss_scaled",
+                "L_AUXlossCos",
+                "L_AUXlossKL",
+                "L_LRclamp",
+                "L_tempClamp",
+                "L_repPenClamp",
+                "L_repLoss",
+                "L_triesLoss",
+                "L_perfLoss",
+                "L_entropyLoss",
+
 
             # LOGIT STATS
                                                                    "6L_0_activationsTensor_norm", # MATCHES 5B_x_finalNormLayer_norm
@@ -388,7 +406,7 @@ reflectionFreq = 10000
 stableFallThreshold = 10 # min 2 cause loss delta is a turn behind lol
 perfectionistRun = True
 # --- #
-trainingDataPairNumber = 10 #169420
+trainingDataPairNumber = 100 #169420
 trainingDataStride = 1
 trainingStartIndex = 0     # // 'random' (not in babyLLM.py)
 epochs = 1